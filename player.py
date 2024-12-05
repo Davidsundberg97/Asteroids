ffe32d0fbdf76330eed6435f982a8e2549f3bdf9
Davidsundberg97 <davidsundberg97@gmail.com>
2024-12-05 16:55:29 +0100

    Shoot functionality

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -27,6 +29,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -36,4 +39,14 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.rotate(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+    def shoot(self):
+        if self.timer > 0:
+            return
+        else:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
     
